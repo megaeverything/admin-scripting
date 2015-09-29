a355f4ac19b90364bf57a4f7a7af32056b0a188b
megaeverything <sk812@live.com>
2015-09-29 20:34:03 +0000

    Added welcome.sh

---
+++ welcome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#this is my welcome script to properly address welcome
+
+mytitle="Dank Lord"
+myname="Riley"
+hostname=`hostname`
+weekday=`date +%A`
+
+echo ""Welcome to planet $hostname, $mytitle $myname!""
+echo ""Today is $weekday""
